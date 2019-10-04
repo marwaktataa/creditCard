b96e74618667a1a05e23eb9634e4b2a9724d86e0
marwaktataa <marwaktata15@gmail.com>
2019-10-04 10:48:15 +0200

    Updates

---
+++ precache-manifest.f5ebb5b922623d771af3026a62523c24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46b4934d6bed39db2c25cab42b2b050",
+    "url": "/creditCard/index.html"
+  },
+  {
+    "revision": "105af71fe169e42cda94",
+    "url": "/creditCard/static/css/main.d6e202f6.chunk.css"
+  },
+  {
+    "revision": "f425139e8853f55203f9",
+    "url": "/creditCard/static/js/2.7368cf0c.chunk.js"
+  },
+  {
+    "revision": "105af71fe169e42cda94",
+    "url": "/creditCard/static/js/main.8461d75f.chunk.js"
+  },
+  {
+    "revision": "f7be89f743e0238d9cbd",
+    "url": "/creditCard/static/js/runtime-main.c86ddcca.js"
+  }
+]);
